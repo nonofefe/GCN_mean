b6c952441dca8df4457ce5343641963944757412
nonofefe <yojunp8632@gmail.com>
2021-08-03 13:12:58 +0900

    citeseer

--- impl.sh
+++ impl.sh
@@ -8,7 +8,7 @@ for ((i = 0; i < ${#array[@]}; i++)) {
     do
         b=`echo "scale=1; $a / 10 " | bc`
         echo $b >> log.txt
-        python run_node_cls.py --rate $b --type ${array[i]} --dataset cora --model neighbor
+        python run_node_cls.py --rate $b --type ${array[i]} --dataset citeseer --model neighbor
         a=`expr $a + 1`
     done
     t=`expr $t + 1`
