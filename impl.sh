febd6425ece097e47e6b0f469ea7dcb1576aaaa0
nonofefe <yojunp8632@gmail.com>
2021-08-20 15:27:17 +0900

    シェルの修正

--- impl.sh
+++ impl.sh
@@ -5,13 +5,13 @@ for ((i = 0; i < ${#array[@]}; i++)) {
     echo "type = ${array[i]}" >> log.txt
     a=0
     rec=1
-    while [ $a -lt 8 ]
+    while [ $a -lt 10 ]
     do
         b=`echo "scale=1; $a / 10 " | bc`
         b=0.8
-        rec=`expr 2 \* $rec`
         echo $rec >> log.txt
         python run_node_cls.py --rate $b --type ${array[i]} --dataset cora --model recursive --rec $rec
+        rec=`expr 2 \* $rec`
         a=`expr $a + 1`
     done
     t=`expr $t + 1`
