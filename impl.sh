ffd984a421af00c1447eb235392e16029cf0094e
nonofefe <yojunp8632@gmail.com>
2021-09-02 00:19:24 +0900

    0~1024の再帰を試す(cora,struct)

--- impl.sh
+++ impl.sh
@@ -1,9 +1,13 @@
 declare -a array=()
 declare -a array=("uniform")
 
+
+
 for ((i = 0; i < ${#array[@]}; i++)) {
     echo "type = ${array[i]}" >> log.txt
     a=0
+    echo "0" >> log.txt
+    python run_node_cls.py --rate $b --type ${array[i]} --dataset cora --model recursive --rec 0
     rec=1
     while [ $a -lt 10 ]
     do
