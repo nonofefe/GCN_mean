54161a05a4f21bc7b03a9a92352fcb59dcde413f
nonofefe <yojunp8632@gmail.com>
2021-09-02 20:51:27 +0900

    make_graph

--- make_graph.py
+++ make_graph.py
@@ -26,9 +26,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 #cora
-y = [70.513,72.118,74.71,75.405,76.352,76.378,76.964,77.153,77.348,77.968,77.789]
-#citeseer
-#y = [0.54552,0.56392,0.56033,0.57608,0.58093,0.59694,0.60105,0.60379,0.60181,0.59671,0.61559]
+#y = [70.513,72.118,74.71,75.405,76.352,76.378,76.964,77.153,77.348,77.968,77.789]
+citeseer
+y = [0.54552,0.56392,0.56033,0.57608,0.58093,0.59694,0.60105,0.60379,0.60181,0.59671,0.61559]
 #amaphoto
 #y = [0.906244512,0.910963398,0.91108492,0.912412882,0.909162518,0.908860908,0.90831918,0.908143484,0.908549048,0.908547586,0.909282578]
 #amacomp
@@ -45,6 +45,6 @@ plt.ylim([70,80])
 plt.semilogx(base=2)
 plt.xlabel("the number of recursion", fontsize=20)
 plt.ylabel("Accuracy", fontsize=20)
-plt.title("Cora",fontsize=25)
+plt.title("Citeseer",fontsize=25)
 #plt.show()
-plt.savefig("results/cora.jpg")
+plt.savefig("results/citeseer.jpg")
