528d05d202f34f006c07911ec43276ef6c535b71
nonofefe <yojunp8632@gmail.com>
2021-09-07 10:02:52 +0900

    amaphoto

--- impl.sh
+++ impl.sh
@@ -5,14 +5,14 @@ for ((i = 0; i < ${#array[@]}; i++)) {
     echo "type = ${array[i]}" >> log.txt
     a=0
     echo "0" >> log.txt
-    python run_node_cls.py --rate 0.8 --type ${array[i]} --dataset amacomp --model recursive --rec 0
+    python run_node_cls.py --rate 0.8 --type ${array[i]} --dataset amaphoto --model recursive --rec 0
     rec=1
     while [ $a -lt 10 ]
     do
         b=`echo "scale=1; $a / 11 " | bc`
         b=0.8
         echo $rec >> log.txt
-        python run_node_cls.py --rate $b --type ${array[i]} --dataset amacomp --model recursive --rec $rec
+        python run_node_cls.py --rate $b --type ${array[i]} --dataset amaphoto--model recursive --rec $rec
         rec=`expr 2 \* $rec`
         a=`expr $a + 1`
     done
