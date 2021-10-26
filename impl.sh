62075085bd3b71b670f8e3274eaf373c3e023254
nonofefe <yojunp8632@gmail.com>
2021-10-26 22:20:00 +0900

    0~1024の再帰を試す(cora,uniform)

--- impl.sh
+++ impl.sh
@@ -1,14 +1,20 @@
 declare -a array=()
-declare -a array=("bias")
+declare -a array=("uniform")
+
+
 
 for ((i = 0; i < ${#array[@]}; i++)) {
     echo "type = ${array[i]}" >> log.txt
     a=0
+    echo "0" >> log.txt
+    python run_node_cls.py --rate 0.9 --type ${array[i]} --dataset cora --model recursive --rec 0
+    rec=1
     while [ $a -lt 10 ]
     do
-        b=`echo "scale=1; $a / 10 " | bc`
-        python run_node_cls.py --rate $b --type ${array[i]} --dataset amacomp --model neighbor
-        #python run_link_pred.py --rate $b --type ${array[i]} --dataset cora
+        b=0.9
+        echo $rec >> log.txt
+        python run_node_cls.py --rate $b --type ${array[i]} --dataset cora --model recursive --rec $rec
+        rec=`expr 2 \* $rec`
         a=`expr $a + 1`
     done
     t=`expr $t + 1`
