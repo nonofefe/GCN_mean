b756c109a5ea451095d43e5f0010b49ec4984456
nonofefe <yojunp8632@gmail.com>
2021-12-06 14:08:03 +0900

    struct citeseer 1:30 0.2

--- impl.sh
+++ impl.sh
@@ -5,13 +5,13 @@ for ((i = 0; i < ${#array[@]}; i++)) {
     echo "type = ${array[i]}" >> log.txt
     a=0
     echo "0" >> log.txt
-    python run_node_cls.py --rate 0.2 --type ${array[i]} --dataset cora --model recursive --rec 0
+    python run_node_cls.py --rate 0.2 --type ${array[i]} --dataset citeseer --model recursive --rec 0
     rec=1
     while [ $a -lt 10 ]
     do
         b=0.2
         echo $rec >> log.txt
-        python run_node_cls.py --rate $b --type ${array[i]} --dataset cora --model recursive --rec $rec
+        python run_node_cls.py --rate $b --type ${array[i]} --dataset citeseer --model recursive --rec $rec
         rec=`expr 2 \* $rec`
         a=`expr $a + 1`
     done
