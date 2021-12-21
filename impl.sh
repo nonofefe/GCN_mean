a5a193e06664dfc416bdf4c67ac690789ac4827b
nonofefe <yojunp8632@gmail.com>
2021-12-21 18:18:20 +0900

    struct 0.8 cora recursive 1:00

--- impl.sh
+++ impl.sh
@@ -1,14 +1,19 @@
 declare -a array=()
-declare -a array=("uniform")
+declare -a array=("struct")
 
 for ((i = 0; i < ${#array[@]}; i++)) {
     echo "type = ${array[i]}" >> log.txt
     a=0
+    b=0.8
+    echo "0" >> log.txt
+    python run_node_cls.py --rate $b --type ${array[i]} --dataset cora --model recursive --rec 0
+    rec=1
     while [ $a -lt 10 ]
     do
-        b=`echo "scale=1; $a / 10 " | bc`
-        echo $b >> log.txt
-        python run_link_pred.py --rate $b --type ${array[i]} --dataset cora
+        echo $rec >> log.txt
+        python run_node_cls.py --rate $b --type ${array[i]} --dataset cora --model recursive --rec $rec
+        rec=`expr 2 \* $rec`
         a=`expr $a + 1`
     done
+    t=`expr $t + 1`
 }
