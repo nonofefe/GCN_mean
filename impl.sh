81db6648150e0faad9cc58bcd8e287f8f98dce74
nonofefe <yojunp8632@gmail.com>
2022-01-13 21:55:58 +0900

    link_pred cora uniform

--- impl.sh
+++ impl.sh
@@ -1,19 +1,14 @@
 declare -a array=()
-declare -a array=("struct")
+declare -a array=("uniform")
 
 for ((i = 0; i < ${#array[@]}; i++)) {
-  echo "type = ${array[i]}" >> log.txt
-  a=0
-  b=0.8
-  echo "0" >> log.txt
-  python run_node_cls.py --rate $b --type ${array[i]} --dataset citeseer --model recursive --rec 0
-  rec=1
-  while [ $a -lt 10 ]
-  do
-    echo $rec >> log.txt
-    python run_node_cls.py --rate $b --type ${array[i]} --dataset citeseer --model recursive --rec $rec
-    rec=`expr 2 \* $rec`
-    a=`expr $a + 1`
-  done
-  t=`expr $t + 1`
+    echo "type = ${array[i]}" >> log.txt
+    a=0
+    while [ $a -lt 10 ]
+    do
+        b=`echo "scale=1; $a / 10 " | bc`
+        echo $b >> log.txt
+        python run_link_pred.py --rate $b --type ${array[i]} --dataset cora
+        a=`expr $a + 1`
+    done
 }
