8af83c9b653cf9b13b695380eb1227e73e65cbaa
nonofefe <yojunp8632@gmail.com>
2022-01-14 22:12:06 +0900

    rec 30忘れてたw

--- impl.sh
+++ impl.sh
@@ -8,7 +8,7 @@ for ((i = 0; i < ${#array[@]}; i++)) {
     do
         b=`echo "scale=1; $a / 10 " | bc`
         echo $b >> log.txt
-        python run_node_cls.py --rate $b --type ${array[i]} --dataset cora --model recursive --epoch 200 --patience 10 --lr 0.01
+        python run_node_cls.py --rate $b --type ${array[i]} --dataset cora --model recursive --epoch 200 --patience 10 --lr 0.01 --rec 30
         a=`expr $a + 1`
     done
     t=`expr $t + 1`
@@ -21,7 +21,7 @@ for ((i = 0; i < ${#array[@]}; i++)) {
     do
         b=`echo "scale=1; $a / 10 " | bc`
         echo $b >> log.txt
-        python run_node_cls.py --rate $b --type ${array[i]} --dataset citeseer --model recursive --epoch 200 --patience 10 --lr 0.01
+        python run_node_cls.py --rate $b --type ${array[i]} --dataset citeseer --model recursive --epoch 200 --patience 10 --lr 0.01 --rec 30
         a=`expr $a + 1`
     done
     t=`expr $t + 1`
