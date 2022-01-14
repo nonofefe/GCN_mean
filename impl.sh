ef7300b1242f0de9a83123707c62c2f106be3025
nonofefe <yojunp8632@gmail.com>
2022-01-14 16:14:27 +0900

    link_pred cora citeseer neighborモデル

--- impl.sh
+++ impl.sh
@@ -12,3 +12,15 @@ for ((i = 0; i < ${#array[@]}; i++)) {
         a=`expr $a + 1`
     done
 }
+
+for ((i = 0; i < ${#array[@]}; i++)) {
+    echo "type = ${array[i]}" >> log.txt
+    a=0
+    while [ $a -lt 10 ]
+    do
+        b=`echo "scale=1; $a / 10 " | bc`
+        echo $b >> log.txt
+        python run_link_pred.py --rate $b --type ${array[i]} --dataset citeseer --rec 30 --lr 0.01 --epoch 200
+        a=`expr $a + 1`
+    done
+}
