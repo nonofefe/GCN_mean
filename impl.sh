18f59f7c6797933caf6ff97b7f4fca598be27d50
nonofefe <yojunp8632@gmail.com>
2022-01-14 20:44:55 +0900

    citeseer

--- impl.sh
+++ impl.sh
@@ -8,20 +8,7 @@ for ((i = 0; i < ${#array[@]}; i++)) {
     do
         b=`echo "scale=1; $a / 10 " | bc`
         echo $b >> log.txt
-        python run_node_cls.py --rate $b --type ${array[i]} --dataset cora --model neighbor --epoch 200 --patience 10 --lr 0.01
-        a=`expr $a + 1`
-    done
-    t=`expr $t + 1`
-}
-
-for ((i = 0; i < ${#array[@]}; i++)) {
-    echo "type = ${array[i]}" >> log.txt
-    a=0
-    while [ $a -lt 10 ]
-    do
-        b=`echo "scale=1; $a / 10 " | bc`
-        echo $b >> log.txt
-        python run_node_cls.py --rate $b --type ${array[i]} --dataset cora --model neighbor --epoch 200 --patience 10 --lr 0.01
+        python run_node_cls.py --rate $b --type ${array[i]} --dataset citeseer --model neighbor --epoch 200 --patience 10 --lr 0.01
         a=`expr $a + 1`
     done
     t=`expr $t + 1`
