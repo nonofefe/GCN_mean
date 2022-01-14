b2a758b175f7a94772bb9f790b316aa86863619d
nonofefe <yojunp8632@gmail.com>
2022-01-14 18:55:37 +0900

    node_cls ハイパラ修正 cora citeseer

--- impl.sh
+++ impl.sh
@@ -1,21 +1,26 @@
 declare -a array=()
-declare -a array=("bias")
-
-dataset="amaphoto"
+declare -a array=("uniform" "bias" "struct")
 
 for ((i = 0; i < ${#array[@]}; i++)) {
     echo "type = ${array[i]}" >> log.txt
     a=0
-    b=0.2
-    echo "0" >> log.txt
-    python run_node_cls.py --rate $b --type ${array[i]} --dataset $dataset --model recursive --rec 0
-    rec=1
     while [ $a -lt 10 ]
     do
-        echo $rec >> log.txt
-        python run_node_cls.py --rate $b --type ${array[i]} --dataset $dataset --model recursive --rec $rec
-        rec=`expr 2 \* $rec`
+        b=`echo "scale=1; $a / 10 " | bc`
+        echo $b >> log.txt
+        python run_node_cls.py --rate $b --type ${array[i]} --dataset $cora --rec 30 --lr 0.01 --epoch 200 --patience 10
         a=`expr $a + 1`
     done
-    t=`expr $t + 1`
+}
+
+for ((i = 0; i < ${#array[@]}; i++)) {
+    echo "type = ${array[i]}" >> log.txt
+    a=0
+    while [ $a -lt 10 ]
+    do
+        b=`echo "scale=1; $a / 10 " | bc`
+        echo $b >> log.txt
+        python run_node_cls.py --rate $b --type ${array[i]} --dataset citeseer --rec 30 --lr 0.01 --epoch 200 --patience 10
+        a=`expr $a + 1`
+    done
 }
